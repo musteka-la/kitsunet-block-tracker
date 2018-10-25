affde934457624af1cac7162bc480677d5db034a
Dmitriy Ryajov <dryajov@gmail.com>
2018-10-25 01:49:53 -0600

    feat: cleanup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const assert = require('assert')
 const EventEmitter = require('events')
 const hexUtils = require('./hex-utils')
+const pify = require('pify')
 
 const log = require('debug')('kitsunet:block-tracker')
 
@@ -15,34 +16,13 @@ class BlockTracker extends EventEmitter {
     assert(node, `libp2p node is required`)
 
     this.node = node
+    this.multicast = pify(this.node.multicast)
     this.ethProvider = provider
-    this.ethQuery = ethQuery
+    this.ethQuery = ethQuery ? pify(ethQuery) : null
     this.topic = topic || DEFAULT_TOPIC
     this.started = false
     this.currentBlock = null
     this.blocks = new Map()
-
-    this.node.multicast.addFrwdHooks(this.topic, [(peer, msg, cb) => {
-      let block = null
-      try {
-        block = JSON.parse(msg.data.toString())
-        if (!block) {
-          return cb(new Error(`No block in message!`))
-        }
-      } catch (err) {
-        log(err)
-        return cb(err)
-      }
-
-      const peerBlocks = this.blocks.has(peer.info.id.toB58String()) || new Set()
-      if (peerBlocks.has(block.number)) {
-        const msg = `already forwarded to peer, skipping block ${block.number}`
-        log(msg)
-        return cb(msg)
-      }
-      peerBlocks.add(block.number)
-      return cb(null, msg)
-    }])
   }
 
   getCurrentBlock () {
@@ -55,20 +35,39 @@ class BlockTracker extends EventEmitter {
       .then(() => this.currentBlock))
   }
 
-  getBlockByNumber (blockNumber) {
+  async getBlockByNumber (blockNumber) {
     log(`latest block is: ${Number(blockNumber)}`)
     const cleanHex = hexUtils.formatHex(blockNumber)
-    this.ethQuery.getBlockByNumber(cleanHex, false, (err, block) => {
-      if (err) {
-        log(err)
-        return
-      }
-      this._publish(Buffer.from(JSON.stringify(block)))
-    })
+    const block = await this.ethQuery.getBlockByNumber(cleanHex, false)
+    this._publish(Buffer.from(JSON.stringify(block)))
   }
 
-  start () {
+  _hook (peer, msg, cb) {
+    let block = null
+    try {
+      block = JSON.parse(msg.data.toString())
+      if (!block) {
+        return cb(new Error(`No block in message!`))
+      }
+    } catch (err) {
+      log(err)
+      return cb(err)
+    }
+
+    const peerBlocks = this.blocks.has(peer.info.id.toB58String()) || new Set()
+    if (peerBlocks.has(block.number)) {
+      const msg = `already forwarded to peer, skipping block ${block.number}`
+      log(msg)
+      return cb(msg)
+    }
+    peerBlocks.add(block.number)
+    return cb(null, msg)
+  }
+
+  async start () {
     if (!this.started) {
+      this.multicast.addFrwdHooks(this.topic, [this._hook.bind(this)])
+
       this.node.multicast.subscribe(this.topic, this._handler.bind(this), () => { })
 
       if (!this.ethProvider) {
@@ -79,9 +78,11 @@ class BlockTracker extends EventEmitter {
     }
   }
 
-  stop () {
+  async stop () {
     if (this.started) {
       this.node.multicast.unsubscribe(this.topic, this._handler.bind(this), () => {})
+      this.multicast.removeFrwdHooks(this.topic, [this._hook.bind(this)])
+
       if (!this.ethProvider) {
         return log(`no eth provider, skipping block tracking`)
       }
